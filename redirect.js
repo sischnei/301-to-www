a3f0af123fc3a18778059f0db96c30a6f781b73b
Simon Schneider <simon.schneider@tridex.net>
2016-08-17 22:02:21 +0200

    Reverse redirect

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
 require('http').createServer(function (request, response) {
-  response.writeHead(301, {"Location": "http://" + request.headers.host + request.url});
+  response.writeHead(301, {"Location": "http://" + request.headers.host.replace("www.", "") + request.url});
   response.end();
 }).listen(80);
